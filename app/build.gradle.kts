abac4571a7d7bc92396351452b150c8c329b5876
KarthikeyankalathiSi <karthikeyankalathi.s@vuedata.in>
2024-11-04 11:03:01 +0530

    bottom sheet dialog update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation("com.google.android.material:material:1.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
